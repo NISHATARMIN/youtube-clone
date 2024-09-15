168a5809a40507f6f7426a7997f590bf29a85e92
NISHATARMIN <nishatarmin34@gmail.com>
2024-09-15 22:32:19 +0530

    Create videolist.js

---
+++ videolist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function VideoList() {
+  return (
+    <ul>
+      <li>Video 1</li>
+      <li>Video 2</li>
+      <li>Video 3</li>
+      <li>Video 4</li>
+    </ul>
+  );
+}
+
+export default VideoList;
